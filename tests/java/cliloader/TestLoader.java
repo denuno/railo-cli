2aaae6ad54e972f8c3ba64ccada8b358e2a2554b
denuno <valliantster@gmail.com>
2014-06-21 12:55:44 -0600

    added a bit of a test

---
+++ tests/java/cliloader/TestLoader.java
@@ -0,0 +1,37 @@
+package cliloader;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestLoader {
+
+	private LoaderCLIMain cliLoader;
+
+	@Before
+	public void setUp() throws Exception {
+		cliLoader = new LoaderCLIMain();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@SuppressWarnings("static-access")
+	@Test
+	public final void testGetPathRoot() {
+		String root = cliLoader.getPathRoot("C:\\Users\\someone\\home");
+		assertTrue(root.equals("C:\\"));
+		root = cliLoader.getPathRoot("C:/Users/someone/home");
+		assertTrue(root.equals("C:/"));
+		root = cliLoader.getPathRoot("C:/Users/someone/home");
+		assertTrue(root.equals("C:/"));
+		root = cliLoader.getPathRoot("/wee/hoo");
+		assertTrue(root.equals("/"));
+	}
+
+}
